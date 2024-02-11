01dd7011208c5aab71e8228fa5890de294d97001
RenatoGalizzi <regalizzi@gmail.com>
2024-02-11 16:08:03 -0300

    add cv

--- Portfolio/src/components/Contact/Contact.jsx
+++ Portfolio/src/components/Contact/Contact.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className='text-center bg-secondary h-100 p-5 text-center mt-5 '>
-            <h2 className='text-white opacity-100'>Under Construction...Here you can send me an email...</h2>
+        <div className='text-center p-5 text-center mt-5 w-25 mx-auto rounded-3 bg-success d-flex bg-dark'>
+            {/* <h2 className='text-white opacity-100'>Under Construction...Here you can send me an email...</h2> */}
+            <div className="d-flex flex-column w-50 mx-auto">
+                <button type="button" className="btn btn-lg btn-light mt-3">
+                <a href="/Renato Galizzi.pdf" download="Renato Galizzi.pdf" target="_blank" className="text-dark link-danger">Descargar CV</a>
+                </button>
+                <button type="button" className="btn btn-lg btn-light mt-3">
+                <a href="/CV Renato Galizzi.pdf" download="Renato Galizzi.pdf" target="_blank" className="text-dark link-danger">Download CV</a>
+                </button>
+            </div>
         </div>
     );
 };
